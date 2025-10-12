8af700dbbcb7fc46e4057353f8a8cf8b3cd9a066
Matt Pocock <mattpocockvoice@gmail.com>
2025-10-12 15:05:53 +0100

    Fixed a bug where visiting non-homepages would show an empty screen

---
+++ .changeset/tasty-cups-chew.md
@@ -0,0 +1,5 @@
+---
+"evalite": patch
+---
+
+Fixed a bug where visiting non-homepages would show an empty screen.
--- apps/evalite-ui/vite.config.ts
+++ apps/evalite-ui/vite.config.ts
@@ -7,7 +7,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [
     TanStackRouterVite({
       autoCodeSplitting: true,
