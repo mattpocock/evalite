4f69de69be133f867c314d87b692ec1599bc09ce
Dylan Mozlowski <dylan.mozlowski@gmail.com>
2025-10-18 15:33:44 -0700

    Increase timeout and limit wokers for evalite-tests

--- packages/evalite-tests/vitest.config.ts
+++ packages/evalite-tests/vitest.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     maxConcurrency: process.env.CI ? 1 : 5,
+    maxWorkers: "50%",
+    testTimeout: 10000,
   },
 });
