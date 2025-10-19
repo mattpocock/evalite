e586e612a40640edaff10ca730ca4f8cde9d5a38
Matt Pocock <mattpocockvoice@gmail.com>
2025-10-19 13:06:22 +0100

    Fixed CI properly

--- packages/evalite/src/backend-only-constants.ts
+++ packages/evalite/src/backend-only-constants.ts
@@ -1,6 +1,7 @@
 // Constants that cannot be used in frontend code
 
 import path from "node:path";
-import { CACHE_LOCATION } from "./constants.js";
+
+const CACHE_LOCATION = path.join("node_modules", ".evalite");
 
 export const FILES_LOCATION = path.join(CACHE_LOCATION, "files");
