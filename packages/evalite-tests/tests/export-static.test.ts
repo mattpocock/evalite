b6d58a461a0c5e399f091ed64e2bced3217fe97b
Matt Pocock <mattpocockvoice@gmail.com>
2025-10-19 12:44:47 +0100

    Changed default storage to in-memory. SQLite still available via config.

--- packages/evalite-tests/tests/export-static.test.ts
+++ packages/evalite-tests/tests/export-static.test.ts
@@ -1,12 +1,12 @@
+import type { Evalite } from "evalite";
 import { exportStaticUI } from "evalite/export-static";
+import { createInMemoryStorage } from "evalite/in-memory-storage";
+import { runEvalite } from "evalite/runner";
+import { createSqliteStorage } from "evalite/sqlite-storage";
 import { readdir, readFile } from "node:fs/promises";
 import path from "node:path";
 import { assert, expect, it } from "vitest";
 import { getSuitesAsRecordViaStorage, loadFixture } from "./test-utils.js";
-import { createInMemoryStorage } from "evalite/in-memory-storage";
-import { createSqliteStorage } from "evalite/sqlite-storage";
-import { runEvalite } from "evalite/runner";
-import type { Evalite } from "evalite";
 
 it("Should export all required files and directory structure", async () => {
   await using fixture = await loadFixture("export");
