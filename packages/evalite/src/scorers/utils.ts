19bc8d50c744beef5ea3427ab2f79e6c75335cbb
Can Temizyurek <can@temizyurek.com>
2025-10-22 11:38:57 -0400

    refactor: Update Evalite types to support userInput structure. And add Multi Turn Sample Type

--- packages/evalite/src/scorers/utils.ts
+++ packages/evalite/src/scorers/utils.ts
@@ -1,6 +1,28 @@
 import type { Evalite } from "../types.js";
+import type { ModelMessage, UserModelMessage } from "ai";
 
-export function isSingleTurnSample(sample: Evalite.Scorers.EvaluationSample) {
-  if ("query" in sample) return true;
+export function isSingleTurnSample(
+  sample: Evalite.Scorers.EvaluationSample
+): sample is Evalite.Scorers.SingleTurnSample {
+  if (Array.isArray(sample.userInput)) return false;
+  return true;
+}
+
+export function isMultiTurnSample(
+  sample: Evalite.Scorers.EvaluationSample
+): sample is Evalite.Scorers.MultiTurnSample {
+  if (Array.isArray(sample.userInput)) return true;
   return false;
+}
+
+export function messageContent(message: ModelMessage) {
+  let content = "";
+  for (const part of message.content) {
+    if (typeof part === "string") {
+      content += part;
+    } else if (part.type === "text") {
+      content += part.text;
+    }
+  }
+  return content;
 }
