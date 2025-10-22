e9aabbcbd520a6cb19fb62026e5bcf67f3d4cf2e
Can Temizyurek <can@temizyurek.com>
2025-10-22 10:34:41 -0400

    refactor: Move SingleTurnSample and EvaluationSample types to Scorers namespace for better organization

--- packages/evalite/src/scorers/utils.ts
+++ packages/evalite/src/scorers/utils.ts
@@ -1,6 +1,6 @@
 import type { Evalite } from "../types.js";
 
-export function isSingleTurnSample(sample: Evalite.EvaluationSample) {
+export function isSingleTurnSample(sample: Evalite.Scorers.EvaluationSample) {
   if ("query" in sample) return true;
   return false;
 }
