9f3aa48e0ac55b5ffdd57842b2f55c171a4aae44
Can Temizyurek <can@temizyurek.com>
2025-10-22 00:46:20 -0400

    feat: Add Context Recall Scorer
    - Introduced a new `contextRecall` scorer to evaluate how much of a generated answer can be attributed to retrieved contexts.
    - Updated the `scorers` module to export the new `contextRecall` scorer.
    
    Part of #250

--- packages/evalite/src/scorers/index.ts
+++ packages/evalite/src/scorers/index.ts
@@ -3,3 +3,4 @@ export type { LLMBasedScorer, EmbeddingBasedScorer } from "./base.js";
 
 export { faithfulness } from "./faithfulness.js";
 export { answerSimilarity } from "./answer-similarity.js";
+export { contextRecall } from "./context-recall.js";
