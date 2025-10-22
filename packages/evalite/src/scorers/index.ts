8868807c574af357869d8c91c53323b1fb8033a5
Can Temizyurek <can@temizyurek.com>
2025-10-22 11:46:16 -0400

    refactor: Export utility functions for sample type checks in scorers

--- packages/evalite/src/scorers/index.ts
+++ packages/evalite/src/scorers/index.ts
@@ -3,3 +3,5 @@ export { createLLMBasedScorer, createEmbeddingBasedScorer } from "./base.js";
 export { faithfulness } from "./faithfulness.js";
 export { answerSimilarity } from "./answer-similarity.js";
 export { contextRecall } from "./context-recall.js";
+
+export { isSingleTurnSample, isMultiTurnSample } from "./utils.js";
