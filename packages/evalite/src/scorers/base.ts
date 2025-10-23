4599c72bf94a8d7d5d057f89a3efdaa2e4f038a1
Can Temizyurek <can@temizyurek.com>
2025-10-22 23:27:36 -0400

    refactor: Consolidate scorer creation functions and enhance structure

--- packages/evalite/src/scorers/base.ts
+++ packages/evalite/src/scorers/base.ts
@@ -1,20 +1,76 @@
 import type { Evalite } from "../types.js";
-import type { EmbeddingModel, LanguageModel } from "ai";
+import type { EmbeddingModel, LanguageModel, ModelMessage } from "ai";
+import { createScorer } from "../create-scorer.js";
+import { isMultiTurn, isSingleTurn } from "./utils.js";
 
-export function createLLMBasedScorer<TInput extends object = {}>(
-  fn: (opts: { model: LanguageModel } & TInput) => Evalite.Scorers.Scorer
-): Evalite.Scorers.LLMBased<TInput> {
-  return (opts) => {
-    return fn(opts);
-  };
+interface BaseScorerOpts<TInput extends object = {}> {
+  name: string;
+  description: string;
+  singleTurn?: (
+    opts: {
+      input: string;
+      output: string;
+      expected: Evalite.Scorers.SingleTurnData;
+    } & TInput
+  ) => Evalite.MaybePromise<Evalite.Scorers.BaseResult>;
+  multiTurn?: (
+    opts: {
+      input: ModelMessage[];
+      output: string;
+      expected: Evalite.Scorers.MultiTurnData;
+    } & TInput
+  ) => Evalite.MaybePromise<Evalite.Scorers.BaseResult>;
 }
 
-export function createEmbeddingBasedScorer<TInput extends object = {}>(
-  fn: (
-    opts: { embeddingModel: EmbeddingModel } & TInput
-  ) => Evalite.Scorers.Scorer
-): Evalite.Scorers.EmbeddingBased<TInput> {
-  return (opts) => {
-    return fn(opts);
-  };
+export function createBaseScorer<TInput extends object = {}>({
+  name,
+  description,
+  singleTurn,
+  multiTurn,
+}: BaseScorerOpts<TInput>): Evalite.Scorers.BaseFactory<TInput> {
+  return (opts: TInput) =>
+    createScorer<
+      string | ModelMessage[],
+      string,
+      Evalite.Scorers.SingleTurnData | Evalite.Scorers.MultiTurnData
+    >({
+      name,
+      description,
+      scorer: async (evalOpts) => {
+        if (isSingleTurn(evalOpts)) {
+          if (!singleTurn)
+            throw new Error("This scorer does not support single turn inputs");
+
+          return singleTurn({
+            ...evalOpts,
+            expected:
+              evalOpts.expected ?? ({} as Evalite.Scorers.SingleTurnData),
+            ...opts,
+          });
+        } else if (isMultiTurn(evalOpts)) {
+          if (!multiTurn)
+            throw new Error("This scorer does not support multi turn inputs");
+
+          return multiTurn({
+            ...evalOpts,
+            expected:
+              evalOpts.expected ?? ({} as Evalite.Scorers.MultiTurnData),
+            ...opts,
+          });
+        }
+        throw new Error("Invalid scorer options");
+      },
+    });
+}
+
+export function createLLMScorer(
+  opts: BaseScorerOpts<{ model: LanguageModel }>
+): Evalite.Scorers.LLMBasedFactory {
+  return createBaseScorer(opts);
+}
+
+export function createEmbeddingScorer(
+  opts: BaseScorerOpts<{ embeddingModel: EmbeddingModel }>
+): Evalite.Scorers.EmbeddingBasedFactory {
+  return createBaseScorer(opts);
 }
--- packages/evalite/src/scorers/utils.ts
+++ packages/evalite/src/scorers/utils.ts
@@ -1,28 +1,26 @@
 import type { Evalite } from "../types.js";
 import type { ModelMessage, UserModelMessage } from "ai";
 
-export function isSingleTurnSample(
-  sample: Evalite.Scorers.EvaluationSample
-): sample is Evalite.Scorers.SingleTurnSample {
-  if (Array.isArray(sample.userInput)) return false;
-  return true;
+export function isSingleTurn(
+  opts: Evalite.ScoreInput<
+    string | ModelMessage[],
+    string,
+    Evalite.Scorers.SingleTurnData | Evalite.Scorers.MultiTurnData
+  >
+): opts is Evalite.ScoreInput<string, string, Evalite.Scorers.SingleTurnData> {
+  return typeof opts.input === "string";
 }
 
-export function isMultiTurnSample(
-  sample: Evalite.Scorers.EvaluationSample
-): sample is Evalite.Scorers.MultiTurnSample {
-  if (Array.isArray(sample.userInput)) return true;
-  return false;
-}
-
-export function messageContent(message: ModelMessage) {
-  let content = "";
-  for (const part of message.content) {
-    if (typeof part === "string") {
-      content += part;
-    } else if (part.type === "text") {
-      content += part.text;
-    }
-  }
-  return content;
+export function isMultiTurn(
+  opts: Evalite.ScoreInput<
+    string | ModelMessage[],
+    string,
+    Evalite.Scorers.SingleTurnData | Evalite.Scorers.MultiTurnData
+  >
+): opts is Evalite.ScoreInput<
+  ModelMessage[],
+  string,
+  Evalite.Scorers.MultiTurnData
+> {
+  return Array.isArray(opts.input);
 }
