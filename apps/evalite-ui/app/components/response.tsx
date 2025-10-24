84b14e485faf09832cfcb367800a1adfd79e546a
Can Temizyurek <can@temizyurek.com>
2025-10-24 11:35:24 -0400

    refactor: change codeblocks theme to dark+ and light+

--- apps/evalite-ui/app/components/response.tsx
+++ apps/evalite-ui/app/components/response.tsx
@@ -7,6 +7,7 @@ type ResponseProps = ComponentProps<typeof Streamdown>;
 export const Response = memo(
   ({ className, ...props }: ResponseProps) => (
     <Streamdown
+      shikiTheme={["light-plus", "dark-plus"]}
       className={cn(
         "w-full [&>*:first-child]:mt-0 [&>*:last-child]:mb-0",
         className
