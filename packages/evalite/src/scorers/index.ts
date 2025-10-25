a731f2d88f91ad56b3f322fb1e61b88c7ce70da9
Can Temizyurek <can@temizyurek.com>
2025-10-24 23:28:47 -0400

    feat: Implement Tool Call Accuracy scorer
    Related #250

--- packages/evalite/src/scorers/index.ts
+++ packages/evalite/src/scorers/index.ts
@@ -3,3 +3,4 @@ export { createLLMScorer, createEmbeddingScorer } from "./base.js";
 export { faithfulness } from "./faithfulness.js";
 export { answerSimilarity } from "./answer-similarity.js";
 export { contextRecall } from "./context-recall.js";
+export { toolCallAccuracy } from "./tool-call-accuracy.js";
