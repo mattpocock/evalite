74425f21cdeac6f2bef799d5bd00571826ad6d28
Matt Pocock <mattpocockvoice@gmail.com>
2025-11-02 20:43:18 +0000

    Reduced timeouts to speed up tests

--- packages/evalite-tests/tests/fixtures/config-precedence/test.eval.ts
+++ packages/evalite-tests/tests/fixtures/config-precedence/test.eval.ts
@@ -4,7 +4,7 @@ evalite("Config Precedence Test", {
   data: () => [{ input: "a", expected: "a" }],
   task: async (input) => {
     // Simulate a task that takes some time
-    await new Promise((resolve) => setTimeout(resolve, 100));
+    await new Promise((resolve) => setTimeout(resolve, 10));
     return input;
   },
   scorers: [],
